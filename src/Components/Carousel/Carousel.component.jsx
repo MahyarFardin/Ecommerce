c35b96b8e7e69bc4bbd1536e916e8c1561d602b9
mahyarfardin <mahyarfardinfar@gmail.com>
2022-07-14 12:01:09 +0430

    done with carousel

--- src/Components/Carousel/Carousel.component.jsx
+++ src/Components/Carousel/Carousel.component.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import Swiper from 'swiper';
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 import data from "./data.json";
+import CarouselCard from '../CarouselCard/CarouselCard.component';
+
+import "./Carousel.style.css"
+import '@splidejs/react-splide/css';
 
 
 function Carousel() {
     return (
-        <Swiper>
-        </Swiper>
+        <div className='slider-container'>
+            <Splide options={{
+                perpage: 1,
+                arrows: false,
+                type: "loop",
+                height: "500px",
+                rewind: "true",
+                autoplay: "true",
+                interval: "2000"
+            }}>
+                {data.map(item => {
+                    return (<SplideSlide key={item.id}>
+                        < CarouselCard {...item} />
+                    </SplideSlide>)
+                })}
+            </Splide>
+        </div>
     );
 }
 
--- src/Pages/CategoriesPage/CategoriesPage.page.jsx
+++ src/Pages/CategoriesPage/CategoriesPage.page.jsx
@@ -9,11 +9,11 @@ export default function CategoriesPage() {
     const [categories, setCategories] = useState([])
 
     function handleMouseOver(event) {
-        event.target.style.backgroundColor = "#F9E4D4"
+        event.target.style.backgroundColor = "white"
     }
 
     function handleMouseLeave(event) {
-        event.target.style.backgroundColor = "white"
+        event.target.style.backgroundColor = "#fcf3ea"
     }
 
     useEffect(() => {
@@ -23,7 +23,7 @@ export default function CategoriesPage() {
     }, [])
 
     return (
-        <div>
+        <div className='first-page'>
             <Carousel />
             <div className='category-container'>
                 {categories.map(category => <div onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave} key={Math.random()} className="category">{category}</div>)}
