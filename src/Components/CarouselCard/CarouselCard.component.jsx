bbe0b5a42fc27da66e8e6ec4d4e6d7922a75c622
mahyarfardin <mahyarfardinfar@gmail.com>
2022-07-14 12:25:46 +0430

    first page is done

--- src/Components/CarouselCard/CarouselCard.component.jsx
+++ src/Components/CarouselCard/CarouselCard.component.jsx
@@ -3,7 +3,13 @@ import "./CarouselCard.style.css"
 
 function CarouselCard(props) {
     return (
+        <div>
             <img className='carousel-card' src={props.image} alt="carousel image" />
+            <div className='text'>
+                <h3>{props.title}</h3>
+                <p>{props.paragraph}</p>
+            </div>
+        </div>
     );
 }
 
