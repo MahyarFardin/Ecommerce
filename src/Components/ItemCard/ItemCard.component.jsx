da8f27bec6b1e59fb5b97217470b060dda153d3b
mahyarfardin <mahyarfardinfar@gmail.com>
2022-07-17 12:56:09 +0430

    shop page is done

--- src/Components/ItemCard/ItemCard.component.jsx
+++ src/Components/ItemCard/ItemCard.component.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import "./ItemCard.style.css"
 
 function ItemCard(props) {
+
+    function handleClick(params) {
+        console.log(params);
+    }
     return (
-        <div className='card'>
+        <div onClick={handleClick} className='card'>
             <img src={props.item.image} alt={props.item.title} />
             <div className='texts'>
-                <h5>{props.item.price}</h5>
-                <span>{`${props.item.rating.rate} (${props.item.rating.rate})`}</span>
-                <p>{props.item.title}</p>
+                <h5>{props.item.price + " $"}</h5>
+                <span>{`${props.item.rating.rate} (${props.item.rating.count})`}</span>
             </div>
+            <p>{props.item.title}</p>
+            <button className='item-button'>See the Item</button>
         </div>
     );
 }
