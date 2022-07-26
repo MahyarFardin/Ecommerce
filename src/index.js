9d793c67ce70c0ddcda158e2893e0fda84faf681
MahyarFardin <mahyarfardinfar@gmail.com>
2022-07-26 10:59:34 +0430

    repaired button part

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+root.render( <
+    React.StrictMode >
+    <
+    BrowserRouter >
+    <
+    App / >
+    <
+    /BrowserRouter> <
+    /React.StrictMode>
 );
